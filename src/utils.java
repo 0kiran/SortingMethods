712e6ae93dea39ef3916f710b927dee0e3e3d1d6
kiraq <kirankhambhla@yahoo.com>
2019-01-07 14:35:39 -0500

    Update utils.java

--- src/utils.java
+++ src/utils.java
@@ -10,11 +10,16 @@ public class utils {
         int swaps = -1;
         while(swaps != 0){
             swaps = 0;
-            for(int i = 0; i < arr.length-1; i++){
+            i = 0;
+            end = arr.length-1
+            while(i < end){
                 if(arr[i] > arr[i+1]){
                     swap(arr,i,i+1);
                     swaps++;
                 }
+                end = i;
+                i++;
+                
             }
         }
     }
